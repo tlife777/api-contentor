24fb046c1a901e8341e5c6855db161011fe5b2e7
rafaelnakayama25 <rafael.nakayama@facens.br>
2024-08-20 15:44:26 -0300

    feat: api endpoint for sensor data register

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from contentor.views import register_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/register_data', register_data),
 ]
--- app/contentor/views.py
+++ app/contentor/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from contentor.models import SensorData
 
 # Create your views here.
+
+@csrf_exempt
+def register_data(request):
+    if request.method=='POST':
+        response = {}
+        bateria = request.POST.get('bateria')
+        latitude = request.POST.get('latitude')
+        longitude = request.POST.get('longitude')
+        volume = request.POST.get('volume')
+
+        if bateria==None or latitude==None or longitude==None or volume==None:
+            response = {"detail": "Required fields: bateria, latitude, longitude, volume"}
+            return JsonResponse(response, status=400)
+        
+        try:
+            result = SensorData.objects.create(
+                bateria=bateria,
+                latitude=latitude,
+                longitude=longitude,
+                volume=volume
+            )
+            if result:
+                response = {
+                    'id': result.id
+                }
+                return JsonResponse(response, status=201)
+            else:
+                return JsonResponse(response, status=500)
+        except Exception as err:
+            response = {
+                "error": str(err)
+            }
+            return JsonResponse(response, status=500)
+
+    response = {"detail": f"Method \"{request.method}\" not allowed."}
+    return JsonResponse(response, status=405)
