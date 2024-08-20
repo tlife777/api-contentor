1fb40dc727d53a997b40b296b40e69ab0497f71b
rafaelnakayama25 <rafael.nakayama@facens.br>
2024-08-20 12:19:31 -0300

    feat: created register

--- app/contentor/models.py
+++ app/contentor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class SensorData(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    bateria = models.DecimalField(max_digits=5, decimal_places=2)
+    volume = models.DecimalField(max_digits=5, decimal_places=2)
