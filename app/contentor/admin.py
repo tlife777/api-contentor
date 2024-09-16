d5b7f556a593e33ac8c172f5ba44e18bc02d818a
tlife <111544734+tlife777@users.noreply.github.com>
2024-09-16 11:22:33 -0300

    fix: SensorData admin display

--- app/contentor/admin.py
+++ app/contentor/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from contentor.models import SensorData
 
-# Register your models here.
 
-admin.site.register(SensorData)
+@admin.register(SensorData)
+class SensorDataAdmin(admin.ModelAdmin):
+    # Campos a serem exibidos na lista de objetos na interface administrativa
+    list_display = ("created_at", "latitude", "longitude", "bateria", "volume")
+
+    # Campos que permitirão a filtragem na lateral da interface administrativa
+    list_filter = ("created_at",)
+
+    # Campos pelos quais se pode buscar
+    search_fields = ("latitude", "longitude")
+
+    # Campos apenas para leitura
+    readonly_fields = ("created_at",)
--- app/contentor/models.py
+++ app/contentor/models.py
@@ -8,3 +8,7 @@ class SensorData(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     bateria = models.DecimalField(max_digits=5, decimal_places=2)
     volume = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Dado do Sensor"
+        verbose_name_plural = "Dados do Sensor"
