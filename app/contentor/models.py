330a9f3f00b4ad4dae1f6c86b90ab705bd383a41
tlife <111544734+tlife777@users.noreply.github.com>
2024-11-01 10:36:47 -0300

    fix: created_at var received value

--- app/contentor/models.py
+++ app/contentor/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-# Create your models here.
 
 class SensorData(models.Model):
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = (models.DateTimeField(auto_now=True))
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     bateria = models.DecimalField(max_digits=5, decimal_places=2)
